d600c230a641a322f56945f0c9061c5a73ad76a8
mk <mk@mkdeiMac.local>
2018-02-07 11:14:17 +0800

    safe observe

--- CKFakeCaret/Classes/UITextField+FakeCaret.swift
+++ CKFakeCaret/Classes/UITextField+FakeCaret.swift
@@ -11,6 +11,7 @@ extension UITextField {
     
     private struct AssociatedKeys {
         static var fakeCaret = "fakeCaret"
+        static var safeObservers = "safeObservers"
     }
     
     internal var fakeCaret: CKFakeCaret? {
@@ -24,18 +25,41 @@ extension UITextField {
         }
     }
     
+    private var safeObservers: [ObserverInfo] {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.safeObservers) as? [ObserverInfo] ?? []
+        }
+        set (new) {
+            objc_setAssociatedObject(self, &AssociatedKeys.safeObservers, new, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    func addSafeObserver(observer: NSObject, forKeyPath keyPath: String, options: NSKeyValueObservingOptions) {
+        let observerInfo = ObserverInfo(observer: observer, keypath: keyPath)
+        if !self.safeObservers.contains(observerInfo) {
+            self.safeObservers.append(ObserverInfo(observer: observer, keypath: keyPath))
+            self.addObserver(observer, forKeyPath: keyPath, options: options, context: nil)
+        }
+    }
+    
+    func removeSafeObserver(observer: NSObject, forKeyPath keyPath: String) {
+        let observerInfo = ObserverInfo(observer: observer, keypath: keyPath)
+        if self.safeObservers.contains(observerInfo) {
+            self.safeObservers.remove(at: self.safeObservers.index(of: observerInfo)!)
+            self.removeObserver(observer, forKeyPath: keyPath)
+        }
+    }
+    
 
     public func setupFakeCaret() {
         setFakeCaret()
-        addObserver(self, forKeyPath: "text", options: [.old, .new], context: nil)
+        addSafeObserver(observer: self, forKeyPath: "text", options: [.old,.new])
     }
     
     public func removeFakeCaret() {
-        if fakeCaret != nil {
-            removeObserver(self, forKeyPath: "text")
-        }
         fakeCaret?.removeFromSuperview()
         fakeCaret = nil
+        removeSafeObserver(observer: self, forKeyPath: "text")
     }
 
     func getCursorPosition() -> CGFloat {
@@ -64,4 +88,19 @@ extension UITextField {
         }
     }
     
+}
+
+
+private class ObserverInfo: Equatable {
+    let observer: UnsafeMutableRawPointer
+    let keypath: String
+    
+    init(observer: NSObject, keypath: String) {
+        self.observer = Unmanaged.passUnretained(observer).toOpaque()
+        self.keypath = keypath
+    }
+}
+
+private func ==(lhs: ObserverInfo, rhs: ObserverInfo) -> Bool {
+    return lhs.observer == rhs.observer && lhs.keypath == rhs.keypath
 }
